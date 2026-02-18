fd25c526025ef59f9fa8469f9abf65c1c79db371
mmqmemm <rdgbnirr3418435@163.com>
2026-02-18 17:16:57 +0800

    rembrandt: Kaleidoscope bring up
    Change-Id: Iafc7b62bcdc825e686ea12e41cca01f68143c56e
    Signed-off-by: mmqmemm <rdgbnirr3418435@163.com>

--- device.mk
+++ device.mk
@@ -246,7 +246,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-kscope
 
 PRODUCT_PACKAGES += \
     FrameworkResOverlay \
